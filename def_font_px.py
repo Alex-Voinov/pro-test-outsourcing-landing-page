2aafdff6d512217fe95a2191b51e0369911f6ccd
Voinov <VoinovAlex1900@yandex.ru>
2024-02-21 20:00:43 +0300

    the rotating tile is almost ready on pure js

--- def_font_px.py
+++ def_font_px.py
@@ -1,4 +1,13 @@
-font = int(input())
-r_x = 1920
-r_y = 1080
-print(f'min({round(font*100/r_x, 5)}vw, {round(font*100/r_y, 5)}vh)')
+while True:
+    font = float(input())
+    r_x = 1920
+    r_y = 1080
+    print(f'Для {font}px:')
+    print()
+    print(f'\tmargin-left: {round(font*100/r_x, 5)}vw;')
+    print(f'\twidth: {round(font*100/r_x, 5)}vw;')
+    print()
+    print(f'\tmargin-top: {round(font*100/r_y, 5)}vh;')
+    print(f'\theight: {round(font*100/r_y, 5)}vh;')
+    print()
+    print(f'\tfont-size: min({round(font*100/r_x, 5)}vw, {round(font*100/r_y, 5)}vh);')
--- scripts/from1200.js
+++ scripts/from1200.js
@@ -1,6 +1,38 @@
 const header = document.getElementById('fixedHeader1200');
 const hieght = window.innerHeight;
+const composition_3__widdgets = document.getElementsByClassName('composition_3__tiles');
 
 window.addEventListener('scroll', () => {
     header.style.marginTop = window.scrollY > hieght ? '0' : '';
 })
+
+const flipWiddget = (widdget, isActive) => {
+    const is_active = widdget.getAttribute('data-active-animation');
+    if(isActive && (is_active=='1' || is_active=='3') || !isActive && (is_active=='2' || is_active=='4')) return 0; // если запустили такую же анимацю заново, пока старая такая же не закончилась - не выполнять ей. 
+    else if(is_active=='1' || is_active=='2'){//если зашли с другой анимацией, но старая ещё не закончилась
+        setTimeout(()=>flipWiddget(widdget, isActive), 100); // запустить проверку этой анимации через 100ms
+        return 0;
+    }
+    widdget.setAttribute('data-active-animation', (isActive ? '1' : '2')); //1 - начали  прямую анимацию, 2 - обратную
+    widdget.style.transform = isActive ? 'rotate3d(-1, 1, 0, 180deg)' : '';
+    const h = widdget.offsetHeight;
+    const w = widdget.offsetWidth
+    widdget.style.height = isActive ? String(w) + 'px' : '';
+    widdget.style.width = isActive ? String(h) + 'px' : '';
+    //widdget.style.marginTop = isActive ? '-1.28177vw' : '';
+    //widdget.style.marginLeft = isActive ? '0' : '';
+    setTimeout(() => {
+        widdget.setAttribute('data-active-animation', isActive? '3': '4'); // 3 - провели прямую анимацию, 4 - обратную
+    }, 500)
+
+}
+
+for (let widdget of composition_3__widdgets) {
+    widdget.addEventListener('mouseover', () => {
+        flipWiddget(widdget, true);
+    });
+    widdget.addEventListener('mouseleave', () => {
+        flipWiddget(widdget, false);
+    });
+    ;
+}
