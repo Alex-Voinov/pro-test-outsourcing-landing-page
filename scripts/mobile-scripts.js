62f8ee4fb5ddb55957a2a6002ae5dc2ba24864c1
Voinov Alex <VoinovAlex1900@andex.ru>
2024-03-29 02:08:53 +0300

    the section with maps is fully implemented

--- scripts/mobile-scripts.js
+++ scripts/mobile-scripts.js
@@ -1,4 +1,5 @@
 import { testers } from "./dataAboutTesters.js";
+import { TELEGRAM_LINK, VK_LINK, TELEPHON_NUMBER, EMAIL, LOCATION, SERVIES_LINK, COUESES_LINK, SOFTWARE_TESTER_COURSE, COMPUTER_LITERACY_COURSE_FOR_QA, MESSAGE_TITLE, MESSAGE_TEMPLATE } from "./basicInformation.js";
 
 const hamburger = document.getElementById('mobile__hamburger');
 const closeAsideMenu = document.getElementById('mobile__right_menu__close');
@@ -15,6 +16,13 @@ let isActiveCoursel = false;
 const leftPointer = document.getElementById('mobaile__tester_pointer_left');
 const rightPointer = document.getElementById('mobaile__tester_pointer_right');
 const amountTesters = testers.length;
+
+const transitionToLink = (link) => {
+    const innerFunc = () => {
+        window.location.href = link;
+    }
+    return innerFunc;
+}
 
 const diffNeberByOne = (number) => {
     if (number == amountTesters) {
@@ -90,6 +98,84 @@ const createTesterCard = (number, isActive, leftValue) => {
         newCard.classList.add('active');
     }
 
+    //back 
+    const back = document.createElement('div');
+    back.id = 'mobile__tester_card__back';
+    newCard.appendChild(back);
+
+    //Создаем блок с именем и специализацией в открытой версии
+    const nameBlockOpen = document.createElement('div');
+    nameBlockOpen.classList.add('mobile__tester_card_open__name_block');
+    const nameHeadingOpen = document.createElement('h1');
+    nameHeadingOpen.textContent = tester.name;
+    const specializationParagraphOpen = document.createElement('p');
+    specializationParagraphOpen.textContent = tester.specialization;
+    nameBlockOpen.appendChild(nameHeadingOpen);
+    nameBlockOpen.appendChild(specializationParagraphOpen);
+    back.appendChild(nameBlockOpen);
+
+
+    //Создаем блок откат в открытой версии
+    const backPointer = document.createElement('div');
+    backPointer.classList.add('mobile__tester_card_open__back_block');
+    backPointer.addEventListener('click', () => {
+        front.style.display = '';
+        back.style.opacity = '';
+        setTimeout(() => front.style.opacity = '', 1)
+        setTimeout(() => {
+            front.style.zIndex = '';
+            back.style.zIndex = '';
+            back.style.display = '';
+        }
+            , 300)
+    })
+    const backPoinerImg = document.createElement('img');
+    backPoinerImg.src = 'img/svg/tablet__comp_8__poiner_reversed.svg';
+    backPoinerImg.alt = 'the back button';
+    backPointer.appendChild(backPoinerImg);
+    back.appendChild(backPointer);
+
+    //Создаем текстовый-блок описаний в открытой версии
+    const descriptionBlock = document.createElement('ul');
+    descriptionBlock.classList.add('mobile__tester_card_open__description_block')
+    tester.characteristics.forEach(
+        characteristic => {
+            const characteristicParagraph = document.createElement('li');
+            characteristicParagraph.innerText = characteristic;
+            descriptionBlock.appendChild(characteristicParagraph);
+        }
+    )
+    back.appendChild(descriptionBlock);
+
+    //Создаем текстовый-блок описаний в открытой версии
+    tester.doesOwn.forEach((skillsRow, number) => {
+        const skillsBlock = document.createElement('div');
+        skillsRow.forEach((skill, numberInRow) => {
+            const skillBlock = document.createElement('p');
+            skillBlock.innerText = skill;
+            skillBlock.classList.add(`${tester.borderColor[number][numberInRow]}_line`)
+            skillsBlock.appendChild(skillBlock);
+        })
+        skillsBlock.classList.add('mobile__tester_card_open__skills_block')
+        skillsBlock.style.top = 58 + 5.15 * number + '%';
+        back.appendChild(skillsBlock);
+    })
+
+    //создаем блок с кнопкой
+    const openCardTgBlock = document.createElement('div');
+    openCardTgBlock.classList.add('mobile__tester_card_open__tg_button_block');
+    const tgLogo = document.createElement('img');
+    const openCardButtonToTransition = document.createElement('div');
+    openCardButtonToTransition.classList.add('mobile__tester_card_open__button_transition');
+    openCardButtonToTransition.innerText = 'Забронировать встречу';
+    //openCardButtonToTransition.addEventListener('click', transitionToForm(tester.qualification));
+    back.appendChild(openCardButtonToTransition);
+    tgLogo.src = 'img/svg/tellegram_logo.svg';
+    tgLogo.alt = 'Telegram logotip';
+    openCardTgBlock.appendChild(tgLogo);
+    openCardTgBlock.addEventListener('click', transitionToLink(TELEGRAM_LINK));
+    back.appendChild(openCardTgBlock);
+
     //front 
     const front = document.createElement('div');
     front.id = 'mobile__tester_card__front';
@@ -133,8 +219,14 @@ const createTesterCard = (number, isActive, leftValue) => {
     button.classList.add('mobile__tester_card__button');
     button.id = `mobile__tester_card_${number}__button`;
     button.addEventListener('click', () => {
-        // openVersion.style.display = 'block';
-        //setTimeout(() => openVersion.style.opacity = 1, 1)
+        back.style.display = 'block';
+        front.style.opacity = '0';
+        setTimeout(() => back.style.opacity = '1', 1)
+        setTimeout(() => {
+            back.style.zIndex = '1';
+            front.style.zIndex = '0';
+        }
+            , 300)
     })
     const buttonText = document.createElement('p');
     buttonText.textContent = 'Больше информации';
@@ -147,11 +239,6 @@ const createTesterCard = (number, isActive, leftValue) => {
     button.appendChild(buttonText);
     button.appendChild(buttonIcon);
     front.appendChild(button);
-
-    //back 
-    const back = document.createElement('div');
-    back.id = 'mobile__tester_card__back';
-    newCard.appendChild(back)
 
     return newCard;
 }
